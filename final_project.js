46789a57113f8bfb47b37ef55bbd8135ccf974ad
jefbari <jf.albari@gmail.com>
2026-04-24 17:30:03 +0700

    release

--- final_project.js
+++ final_project.js
@@ -38,5 +38,50 @@ function handleSubmit() {
   textarea.value = ""
 }
 
+function render() {
+  container.innerHTML = ""
 
+  for (let i = 0; i < index; i++) {
+    container.innerHTML += `
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-2">
+            <img src="${articles[i].image}" class="img-fluid rounded-start article-img">
+          </div>
+          <div class="col-md-10">
+            <div class="card-body">
+              <h5 class="card-title">${articles[i].title}</h5>
+              <p class="card-text">
+                <small class="text-body-secondary">
+                  published at: ${articles[i].time}
+                </small>
+              </p>
+              <p class="card-text">${articles[i].content}</p>
+              <button class="btn btn-danger" onclick="deleteArticle(${i})">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  }
+}
 
+function deleteArticle(pos) {
+  for (let i = pos; i < index - 1; i++) {
+    articles[i] = articles[i + 1]
+  }
+
+  index--
+
+  render()
+}
+function myFunction() {
+  var x = document.getElementById("Password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
